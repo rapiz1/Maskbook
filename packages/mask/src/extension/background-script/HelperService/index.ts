f6a9d435cfb213f1f258cb2a66373d978b62829e
Lantt <laanfor@gmail.com>
2022-02-10 11:25:58 +0800

    fix: should without lock when persona sign

--- packages/mask/src/extension/background-script/HelperService/index.ts
+++ packages/mask/src/extension/background-script/HelperService/index.ts
@@ -18,13 +18,20 @@ export async function openPopupWindow(route?: PopupRoutes, params?: Record<strin
     if (popup) {
         await browser.windows.update(currentPopupWindowId.value, { focused: true })
     } else {
+        const exclusionDetectLocked = [PopupRoutes.PersonaSignRequest]
         const locked = await isLocked()
 
-        const url = urlcat('popups.html#', locked ? PopupRoutes.Unlock : route ?? PopupRoutes.Wallet, {
-            toBeClose: 1,
-            from: locked && route ? route : null,
-            ...params,
-        })
+        const url = urlcat(
+            'popups.html#',
+            locked && !exclusionDetectLocked.includes(route ?? PopupRoutes.Wallet)
+                ? PopupRoutes.Unlock
+                : route ?? PopupRoutes.Wallet,
+            {
+                toBeClose: 1,
+                from: locked && route ? route : null,
+                ...params,
+            },
+        )
 
         let left: number
         let top: number
