87278d8f8953f6ed985b794abaac811da7360b05
guanbinrui <guanbinrui@dimension.im>
2022-02-15 15:27:13 +0800

    fix: ts error

--- packages/mask/src/extension/background-script/EthereumServices/middlewares/Logger.ts
+++ packages/mask/src/extension/background-script/EthereumServices/middlewares/Logger.ts
@@ -3,9 +3,9 @@ import type { Context, Middleware } from '../types'
 export class Logger implements Middleware<Context> {
     async fn(context: Context, next: () => Promise<void>) {
         console.log('START!')
-        console.log(`${context.payload.method}_${JSON.stringify(context.payload.params)}`)
+        console.log(`${context.request.method}_${JSON.stringify(context.request.params)}`)
         await next()
         console.log('END!')
-        console.log(`${context.payload.method}_${JSON.stringify(context.payload.params)}`)
+        console.log(`${context.request.method}_${JSON.stringify(context.request.params)}`)
     }
 }
